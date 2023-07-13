c233e5f11ec0bd68cec12a421a7f41990ad7d12d
harry park <harrytucker1016@gmail.com>
2023-07-13 21:10:31 +0900

    댓글 구현 / 데이터베이스 연동 오류 해결해야함

--- src/main/java/com/sparta/springauth/entity/Comment.java
+++ src/main/java/com/sparta/springauth/entity/Comment.java
@@ -1,9 +1,5 @@
 package com.sparta.springauth.entity;
 
-import java.util.List;
-
-import com.sparta.springauth.dto.PostRequestDto;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,7 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,40 +14,37 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
-@Table(name = "post")
-public class Post extends TimeStamped {
+@Table(name = "comment")
+public class Comment extends TimeStamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    private String title;
+    private String body;
 
-    @Column(nullable = false)
-    private String content;
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
-    private List<Comment> comments;
-
-    public Post(PostRequestDto requestDto) {
-        this.title = requestDto.getTitle();
-        this.content = requestDto.getContent();
+    public Comment(String body) {
+        this.body = body;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
+    public void setBody(String body) {
+        this.body = body;
     }
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
     }
 }
